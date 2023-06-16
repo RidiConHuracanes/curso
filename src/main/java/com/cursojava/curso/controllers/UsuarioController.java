ceafbc5c64ff93d5d98bbb2366f763473177495c
RidiConHuracanes <osorionatareno026b@gmail.com>
2023-06-16 16:39:49 -0600

    agregamos archivo Usuario y modificamos a la hora 1.24.37

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,5 +1,8 @@
 package com.cursojava.curso.controllers;
 
+
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,35 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "prueba")
-    public List<String> prueba(){
-        return List.of("Manzana", "Mini", "Banana");
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Rene");
+        usuario.setApellido("Osorio");
+        usuario.setEmail("a@gmail.com");
+        usuario.setTelefono("44867451");
+        return usuario;
     }
+
+    @RequestMapping(value = "usuarioa")
+    public Usuario editarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Rene");
+        usuario.setApellido("Osorio");
+        usuario.setEmail("a@gmail.com");
+        usuario.setTelefono("44867451");
+        return usuario;
+    }
+
+    @RequestMapping(value = "usuariob")
+    public Usuario eliminarUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Rene");
+        usuario.setApellido("Osorio");
+        usuario.setEmail("a@gmail.com");
+        usuario.setTelefono("44867451");
+        return usuario;
+    }
+
 }
