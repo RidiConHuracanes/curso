6e8cfd0b1229700920399d86cc62fe88b9150a6e
RidiConHuracanes <osorionatareno026b@gmail.com>
2023-06-16 18:48:19 -0600

    AGREGAMOS LAS CARPETAS DAO-MODELS

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -1,10 +1,12 @@
 package com.cursojava.curso.controllers;
 
-
+import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.cursojava.curso.utils.JWTUtil;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,65 +14,39 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "usuario/{id}")
-    public Usuario getUsuario(@PathVariable Long id){
-        Usuario usuario = new Usuario();
-        usuario.setId(id);
-        usuario.setNombre("Rene");
-        usuario.setApellido("Osorio");
-        usuario.setEmail("a@gmail.com");
-        usuario.setTelefono("44867451");
-        return usuario;
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.GET)
+    public List<Usuario> getUsuarios(@RequestHeader(value="Authorization") String token) {
+        if (!validarToken(token)) { return null; }
+
+        return usuarioDao.getUsuarios();
     }
 
-    @RequestMapping(value = "usuarios")
-    public List <Usuario> getUsuarios(){
-        List<Usuario> usuarios = new ArrayList<>();
-        Usuario usuario = new Usuario();
-        usuario.setId(23l);
-        usuario.setNombre("Rene");
-        usuario.setApellido("Osorio");
-        usuario.setEmail("a@gmail.com");
-        usuario.setTelefono("44867451");
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(45l);
-        usuario2.setNombre("Alejandro");
-        usuario2.setApellido("Osorio");
-        usuario2.setEmail("b@gmail.com");
-        usuario2.setTelefono("23456789");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(56l);
-        usuario3.setNombre("Rene");
-        usuario3.setApellido("Natareno");
-        usuario3.setEmail("c@gmail.com");
-        usuario3.setTelefono("12345678");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-        return usuarios;
+    private boolean validarToken(String token) {
+        String usuarioId = jwtUtil.getKey(token);
+        return usuarioId != null;
     }
 
-    @RequestMapping(value = "usuarioa")
-    public Usuario editar(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Rene");
-        usuario.setApellido("Osorio");
-        usuario.setEmail("a@gmail.com");
-        usuario.setTelefono("44867451");
-        return usuario;
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
+    public void registrarUsuario(@RequestBody Usuario usuario) {
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        String hash = argon2.hash(1, 1024, 1, usuario.getPassword());
+        usuario.setPassword(hash);
+
+        usuarioDao.registrar(usuario);
     }
 
-    @RequestMapping(value = "usuariob")
-    public Usuario eliminarUsuario(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("Rene");
-        usuario.setApellido("Osorio");
-        usuario.setEmail("a@gmail.com");
-        usuario.setTelefono("44867451");
-        return usuario;
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void eliminar(@RequestHeader(value="Authorization") String token,
+                         @PathVariable Long id) {
+        if (!validarToken(token)) { return; }
+        usuarioDao.eliminar(id);
     }
 
 }
