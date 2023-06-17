2fb941ce9bfc5c4b79837c113472a5e00eb39132
RidiConHuracanes <osorionatareno026b@gmail.com>
2023-06-16 19:09:20 -0600

    correcioness 1

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -2,24 +2,55 @@
 $(document).ready(function() {
     cargarUsuarios();
   $('#usuarios').DataTable();
+  actualizarEmailDelUsuario();
 });
 
+function actualizarEmailDelUsuario() {
+    document.getElementById('txt-email-usuario').outerHTML = localStorage.email;
+}
 
-async function cargarUsuarios(){
-     const request = await fetch('usuarios', {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        }
-      });
-      const usuarios = await request.json();
 
-      console.log(usuarios);
+async function cargarUsuarios() {
+  const request = await fetch('api/usuarios', {
+    method: 'GET',
+    headers: getHeaders()
+  });
+  const usuarios = await request.json();
 
-    let usuario = '<tr><td>123</td><td>Rene Osorio</td><td>a@gmail.com</td><td>44867451</td><td>Salary</td><td><a href="a" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
-    let a = "a";
 
-document.querySelector('#usuarios tbody').outerHTML = a;
+  let listadoHtml = '';
+  for (let usuario of usuarios) {
+    let botonEliminar = '<a href="#" onclick="eliminarUsuario(' + usuario.id + ')" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a>';
 
+    let telefonoTexto = usuario.telefono == null ? '-' : usuario.telefono;
+    let usuarioHtml = '<tr><td>'+usuario.id+'</td><td>' + usuario.nombre + ' ' + usuario.apellido + '</td><td>'
+                    + usuario.email+'</td><td>'+telefonoTexto
+                    + '</td><td>' + botonEliminar + '</td></tr>';
+    listadoHtml += usuarioHtml;
+  }
+
+document.querySelector('#usuarios tbody').outerHTML = listadoHtml;
+
+}
+
+function getHeaders() {
+    return {
+     'Accept': 'application/json',
+     'Content-Type': 'application/json',
+     'Authorization': localStorage.token
+   };
+}
+
+async function eliminarUsuario(id) {
+
+  if (!confirm('¿Desea eliminar este usuario?')) {
+    return;
+  }
+
+ const request = await fetch('api/usuarios/' + id, {
+    method: 'DELETE',
+    headers: getHeaders()
+  });
+
+  location.reload()
 }
